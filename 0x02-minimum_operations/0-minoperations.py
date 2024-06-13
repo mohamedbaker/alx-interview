ff22265f68101edf40c1f8db210f047d078d3c7b
mohamedbaker <dam.vampire22@gmail.com>
2024-06-14 02:55:02 +0300

    recursin

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-def minOperations(n, memo={}):
+def minOperations(n):
     """
     Calculates the minimum number of operations (Copy All, Paste)
       needed to achieve n H characters.
     Args:
         n: The target number of H characters.
-        memo (dict, optional): Memoization table to store
-        minimum operations for subproblems. Defaults to {}.
     Returns:
         int: The minimum number of operations required, or 0 if impossible.
     """
 
-    if n == 0:
+    if n <= 1:
         return 0
-    elif n == 1:
-        return 1
 
-    if n in memo:
-        return memo[n]
+    memo = {}
 
-    min_ops = float('inf')
-    for j in range(1, n // 2 + 1):
-        remaining_ops = minOperations(n - j, memo)
-        operations = 1 + remaining_ops
-        min_ops = min(min_ops, operations)
+    def helper(n):
+        if n in memo:
+            return memo[n]
 
-    memo[n] = min_ops
+        # Base case
+        if n == 1:
+            return 0
 
-    return min_ops
+        min_ops = float('inf')
+
+        for i in range(2, n + 1):
+            if n % i == 0:
+                min_ops = min(min_ops, helper(n // i) + i)
+
+        memo[n] = min_ops
+        return min_ops
+
+    return helper(n)
